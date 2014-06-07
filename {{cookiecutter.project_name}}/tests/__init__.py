9f12f9d1427255de3375023576cf299aa9779447
Maxim Avanov <maxim.avanov@gmail.com>
2014-06-07 16:18:03 +0400

    Update test templates

--- {{cookiecutter.project_name}}/tests/__init__.py
+++ {{cookiecutter.project_name}}/tests/__init__.py
@@ -3,7 +3,7 @@ import transaction
 
 from pyramid import testing
 
-from .models import DBSession
+from {{ cookiecutter.project_name }}.models import DBSession
 
 
 class TestMyViewSuccessCondition(unittest.TestCase):
@@ -11,7 +11,7 @@ class TestMyViewSuccessCondition(unittest.TestCase):
         self.config = testing.setUp()
         from sqlalchemy import create_engine
         engine = create_engine('sqlite://')
-        from .models import (
+        from {{ cookiecutter.project_name }}.models import (
             Base,
             MyModel,
             )
@@ -26,7 +26,7 @@ class TestMyViewSuccessCondition(unittest.TestCase):
         testing.tearDown()
 
     def test_passing_view(self):
-        from .views import my_view
+        from {{ cookiecutter.project_name }}.views import my_view
         request = testing.DummyRequest()
         info = my_view(request)
         self.assertEqual(info['one'].name, 'one')
@@ -38,7 +38,7 @@ class TestMyViewFailureCondition(unittest.TestCase):
         self.config = testing.setUp()
         from sqlalchemy import create_engine
         engine = create_engine('sqlite://')
-        from .models import (
+        from {{ cookiecutter.project_name }}.models import (
             Base,
             MyModel,
             )
@@ -49,7 +49,7 @@ class TestMyViewFailureCondition(unittest.TestCase):
         testing.tearDown()
 
     def test_failing_view(self):
-        from .views import my_view
+        from {{ cookiecutter.project_name }}.views import my_view
         request = testing.DummyRequest()
         info = my_view(request)
         self.assertEqual(info.status_int, 500)
